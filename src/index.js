a5d10cc0552af5005508f0daf297dc29f09bcf70
Patrick Poulain <patrick@adopteunmec.com>
2019-09-30 14:59:30 +0200

    Use yargs instead of minimist

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
+#!/usr/bin/ env node
+
 var http = require("http");
-var argv = require("minimist")(process.argv.slice(2));
 var util = require("util");
-var port = argv["p"] ? parseInt(argv["p"]) : 80;
-var hostname = argv["h"] ? argv["h"] : "0.0.0.0";
 
 function log(message) {
     process.stdout.write(new Date() + " " + message + "\n");
 }
 
-log(util.format("http server listening on %s:%d", hostname, port));
 var server = http.createServer(function (request, response) {
     log(util.format("request: %j", {
         "headers": request.headers,
@@ -25,22 +23,46 @@ var server = http.createServer(function (request, response) {
         };
         response.writeHead(200, headers);
         response.end(body, "utf8", function () {
-            log(util.format("response: %j", {"headers": headers, "body": body}));
+            log(util.format("response: %j", {
+                "headers": headers,
+                "body": body
+            }));
         });
     }, 1000);
-}).listen(port, hostname);
+})
 
 function logConnections() {
-    server.getConnections(function(err, count) {
+    server.getConnections(function (err, count) {
         log(util.format("connection: %d open", count));
     });
 }
 
-server.on("connection", function(socket) {
+server.on("connection", function (socket) {
     log(util.format("connection: open from %s", socket.remoteAddress));
     logConnections();
-    socket.on("close", function() {
+    socket.on("close", function () {
         log(util.format("connection: closed with %s", socket.remoteAddress));
         logConnections();
     });
 });
+
+require("yargs")
+    .command("$0", "start the http server", (yargs) => {
+        yargs
+            .option("port", {
+                alias: "p",
+                describe: "port to bind on",
+                default: 80
+            })
+            .option("ip", {
+                alias: "i",
+                describe: "ip to bind on",
+                default: "0.0.0.0"
+            })
+    }, (argv) => {
+        log(util.format("http server listening on %s:%d", argv.ip, argv.port));
+        server.listen(argv.port, argv.ip);
+    })
+    .alias('h', 'help')
+    .help('help')
+    .argv
